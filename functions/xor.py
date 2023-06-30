eb4081636e3db6d677631884a40de70f8c847725
Jannis Milz <jannis@milz.ch>
2023-06-30 19:20:30 +0200

    Fix xor Funktion failt bei gewissen Inputs

--- functions/xor.py
+++ functions/xor.py
@@ -12,8 +12,21 @@ def xor(text, key):
     result = []
     # Iteriere über jedes Zeiche vom text drüber und bechum au grad de index (mit enumerate()) devo
     for index, char in enumerate(text):
+        # Mues überprüefe ob er en string isch und denn andersch handle wege de ord() funktion
+        if isinstance(char, str):
+            # De Unicode vom aktuelle Zeiche
+            char_code = ord(char)
+        else:
+            char_code = char
+
+        # Mues überprüefe ob er en string isch und denn andersch handle wege de ord() funktion
+        if isinstance(key[index % len(key)], str):
+            key_code = ord(key[index % len(key)])
+        else:
+            key_code = key[index % len(key)]
+
         # Mach e XOR rechnig zwüsched de Unicode Zahle vom text und enere gwüsse Stell vom key
-        xored_char = ord(char) ^ ord(key[index % len(key)])
+        xored_char = char_code ^ key_code
         # Füeg d'"xored" Zahl zu de Liste
         result.append(chr(xored_char))
 
